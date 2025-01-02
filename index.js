3fe3d3105cc4b837d04034bc6c1bdc6f335ae580
shekokarmahesh <shekokarmahesh@gmail.com>
2025-01-02 16:20:31 +0530

    telegram joke bot

---
+++ index.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+
+dotenv.config();
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+
+// bot.on('message', (option) => {
+//     console.log("Message received on the bot", option);
+
+//     bot.sendMessage(option.chat.id, "Hello, I am a bot. I am here to help you with your queries. Please type /help to know more about me.");
+
+// });
+
+bot.onText(/\/joke/, async (option) => {
+    const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
+
+    console.log(response.data);
+
+    const setup = response.data.setup;
+    const punchline = response.data.punchline;
+
+    bot.sendMessage(option.chat.id, setup + " ,  " + punchline);
+});
